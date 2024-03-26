b9c7af13051f44c868403914a3fa073c5d85b82a
DuaaShareef <75999852+DuaaAlshareef@users.noreply.github.com>
2024-03-26 17:33:42 +0000

    KNN implementation

---
+++ K-Nearest Neighbor/dataset.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import make_moons
+
+"""# Data Generation"""
+
+X, y = make_moons(n_samples=600, noise=0.55, random_state=0)
+
+# shuffle data
+idx = np.random.permutation(X.shape[0])
+X, y = X[idx,:], y[idx]
+
+# train/test split
+ratio = 0.8
+X_train, y_train = X[:int (ratio*X.shape[0])], y[:int (ratio*X.shape[0])].reshape(-1,1)
+X_test, y_test = X[int (ratio*X.shape[0]):], y[int (ratio*X.shape[0]):].reshape(-1,1)
